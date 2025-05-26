5ee998a93528aa1c2ebc2f406f85e5dfa8b0cfb5
git-amit-gupta <amit.gupta@idera.com>
2025-05-26 13:29:23 +0530

    changes in firebase config

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -4,25 +4,25 @@ import { getFirestore, doc, setDoc, getDoc, collection, getDocs } from "firebase
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 // Replace with your Firebase project config
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, // Correct storage bucket URL
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDGmXvrsunTXGGmbTqWCad7I_XLJHaND1g",
-  authDomain: "reext-ecommerce.firebaseapp.com",
-  projectId: "reext-ecommerce",
-  storageBucket: "reext-ecommerce.firebasestorage.app",
-  messagingSenderId: "920272011795",
-  appId: "1:920272011795:web:c3b2e4e1c55dbe327375e6",
-  measurementId: "G-J0P2JK95VS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET, // Correct storage bucket URL
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDGmXvrsunTXGGmbTqWCad7I_XLJHaND1g",
+//   authDomain: "reext-ecommerce.firebaseapp.com",
+//   projectId: "reext-ecommerce",
+//   storageBucket: "reext-ecommerce.firebasestorage.app",
+//   messagingSenderId: "920272011795",
+//   appId: "1:920272011795:web:c3b2e4e1c55dbe327375e6",
+//   measurementId: "G-J0P2JK95VS"
+// };
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
